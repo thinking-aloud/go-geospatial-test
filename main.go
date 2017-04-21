c215ba3d7091f25fafda7a6e432302a19e38fc90
Lennart K <lennart.k@thinking-aloud.eu>
2017-04-21 14:46:35 +0200

    Some refactoring. Geo stuff with golang is not fun, will try python.

--- main.go
+++ main.go
@@ -11,42 +11,68 @@ import (
 	"github.com/paulsmith/gogeos/geos"
 )
 
+var gc *draw2dimg.GraphicContext
+
 func main() {
-	initGeos()
-	initGraphics()
+	dest := initGraphics()
+
+	drawImage("res/white.png")
+	drawPolygon()
+
+	geosTryout()
+	clipShp("res/a.shp", "res/b.shp")
+
+	// Save to file
+	draw2dimg.SaveToPngFile("output.png", dest)
 }
 
-func initGeos() {
-	line, _ := geos.FromWKT("LINESTRING (0 0, 10 10, 20 20)")
-	buf, _ := line.Buffer(2.5)
-	fmt.Println(buf)
-}
-
-func initGraphics() {
+func initGraphics() *image.RGBA {
 	// Initialize the graphic context on an RGBA image
 	dest := image.NewRGBA(image.Rect(0, 0, 400, 400))
-	gc := draw2dimg.NewGraphicContext(dest)
+	gc = draw2dimg.NewGraphicContext(dest)
 
 	// Set some properties
 	gc.SetFillColor(color.RGBA{0x44, 0xff, 0x44, 0xff})
 	gc.SetStrokeColor(color.RGBA{0x44, 0x44, 0x44, 0xff})
 	gc.SetLineWidth(5)
 
-	drawExistingImage(gc, "hello.png")
-	drawShape(gc)
-
-	// Save to file
-	draw2dimg.SaveToPngFile("hello.png", dest)
+	return dest
 }
 
-func drawExistingImage(gc *draw2dimg.GraphicContext, location string) {
-	infile, _ := os.Open(location)
+func geosTryout() {
+	line, _ := geos.FromWKT("LINESTRING (0 0, 10 10, 20 20)")
+	buf, _ := line.Buffer(2.5)
+	fmt.Println(buf)
+}
+
+func clipShp(a string, b string) {
+	var err error
+	shpA, err := os.Open(a)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer shpA.Close()
+
+	//imageA, _, _ := image.Decode(shpA)
+
+}
+
+func drawImage(location string) {
+	infile, err := os.Open(location)
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer infile.Close()
-	src, _, _ := image.Decode(infile)
+
+	src, _, err := image.Decode(infile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	gc.DrawImage(src)
 }
 
-func drawShape(gc *draw2dimg.GraphicContext) {
+func drawPolygon() {
 	gc.MoveTo(10, 10) // should always be called first for a new path
 	gc.LineTo(100, 50)
 	gc.QuadCurveTo(100, 10, 10, 10)
